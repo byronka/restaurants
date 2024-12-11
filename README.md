3a212e99b42e2142d888a3520b774557c954a0d0
Byron Katz <byronka@msn.com>
2024-12-10 20:31:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Restaurants
+===========
+
+Quick start:
+------------
+
+To build and run:  
+
+```shell
+./mvnw compile exec:java
+```
+
+then visit http://localhost:8080
---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static com.renomad.minum.testing.TestFramework.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest
+{
+
+    @Test
+    public void testBasics()
+    {
+        assertTrue( true );
+    }
+
+
+}
