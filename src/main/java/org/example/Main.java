e5b1cf93c88ae10d70432f80515597573f6495a7
Byron Katz <byronka@msn.com>
2024-12-10 21:22:14 -0500

    Rudiments of a sorted list starting to take shape

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,12 @@ import com.renomad.minum.database.Db;
 import com.renomad.minum.state.Context;
 import com.renomad.minum.templating.TemplateProcessor;
 import com.renomad.minum.utils.FileUtils;
+import com.renomad.minum.utils.SearchUtils;
 import com.renomad.minum.web.FullSystem;
 import com.renomad.minum.web.Response;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,16 +25,47 @@ public class Main {
         var fileUtils = new FileUtils(context.getLogger(), context.getConstants());
         Db<Restaurant> restaurants = context.getDb("restaurants", Restaurant.EMPTY);
         TemplateProcessor indexProcessor = TemplateProcessor.buildProcessor(fileUtils.readTextFile("src/main/webapp/templates/index.html"));
+        TemplateProcessor restaurantEntryProcessor = TemplateProcessor.buildProcessor(fileUtils.readTextFile("src/main/webapp/templates/restaurant_entry.html"));
+
         fs.getWebFramework().registerPath(GET, "", request -> {
-            String restaurantsString = restaurants.values().stream().map(x ->  "<li><a href=\"#\">❌</a>"+x.getName()+"<a href=\"#\">\uD83E\uDC71</a> <a href=\"#\">\uD83E\uDC73</a></li>").collect(Collectors.joining("\n"));
+            String restaurantsString = restaurants.values().stream()
+                    .sorted(Comparator.comparingInt(Restaurant::getPlaceInList))
+                    .map(x -> {
+                        Map<String,String> myMap = new HashMap<>();
+                        myMap.put("restaurant_name", x.getName());
+                        myMap.put("restaurant_id", String.valueOf(x.getIndex()));
+                        return restaurantEntryProcessor.renderTemplate(myMap);
+                    }).collect(Collectors.joining("\n"));
 
             return Response.htmlOk(indexProcessor.renderTemplate(Map.of("restaurantsList", restaurantsString)));
         });
+
         fs.getWebFramework().registerPath(POST, "add_restaurant", request -> {
             String restaurantName = request.getBody().asString("restaurant");
             restaurants.write(new Restaurant(0, restaurantName, 0));
             return Response.redirectTo("/");
         });
+
+        fs.getWebFramework().registerPath(POST, "increment_position", request -> {
+            String idString = request.getBody().asString("id");
+            long id = Long.parseLong(idString);
+            Restaurant r = SearchUtils.findExactlyOne(restaurants.values().stream(), x -> x.getIndex() == id);
+            int newPlaceInList = r.getPlaceInList() + 1;
+            restaurants.write(new Restaurant(r.getIndex(), r.getName(), newPlaceInList));
+            return Response.redirectTo("/");
+        });
+
+        fs.getWebFramework().registerPath(POST, "decrement_position", request -> {
+            String idString = request.getBody().asString("id");
+            long id = Long.parseLong(idString);
+            Restaurant r = SearchUtils.findExactlyOne(restaurants.values().stream(), x -> x.getIndex() == id);
+            int newPlaceInList = r.getPlaceInList() - 1;
+            restaurants.write(new Restaurant(r.getIndex(), r.getName(), newPlaceInList));
+            return Response.redirectTo("/");
+        });
+
+
+
         fs.block();
     }
 }
