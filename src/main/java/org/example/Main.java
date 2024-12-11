f642a33516cefe5c3cde29dc3b3187c6694d7529
Byron Katz <byronka@msn.com>
2024-12-10 20:45:37 -0500

    some code in place

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import com.renomad.minum.state.Context;
+import com.renomad.minum.templating.TemplateProcessor;
+import com.renomad.minum.utils.FileUtils;
 import com.renomad.minum.web.FullSystem;
 import com.renomad.minum.web.Response;
+
+import java.util.Map;
 
 import static com.renomad.minum.web.RequestLine.Method.GET;
 
@@ -9,7 +14,10 @@ public class Main {
 
     public static void main(String[] args) {
         FullSystem fs = FullSystem.initialize();
-        fs.getWebFramework().registerPath(GET, "", request -> Response.htmlOk("<p>Hi there world!</p>"));
+        Context context = fs.getContext();
+        var fileUtils = new FileUtils(context.getLogger(), context.getConstants());
+        TemplateProcessor indexProcessor = TemplateProcessor.buildProcessor(fileUtils.readTextFile("src/main/webapp/templates/index.html"));
+        fs.getWebFramework().registerPath(GET, "", request -> Response.htmlOk(indexProcessor.renderTemplate(Map.of())));
         fs.block();
     }
 }
